eeba417cb0ee56b39d3bff705e54e46ce0f4ee15
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-22 03:42:18 +0200

    Add : "AppDBContext Data"

---
+++ OnlineStoreAPI/Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStoreAPI.Models;
+
+namespace OnlineStoreAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categorie> Categories { get; set; }   
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        
+    }
+}
